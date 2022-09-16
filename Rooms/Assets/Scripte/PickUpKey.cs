9259eb275e29bca8eb6525fd163997383bad370f
Abdulelah21 <78698991+Abdulelah21@users.noreply.github.com>
2022-09-16 20:01:38 +0300

    initial commit

---
+++ Rooms/Assets/Scripte/PickUpKey.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpKey : MonoBehaviour
+{
+    public GameObject keyOB;
+    public GameObject invOB;
+    public GameObject pickUpText;
+    public AudioSource keySound;
+
+    public bool inReach;
+
+    private Inventory inventory;
+    public GameObject itemButton;
+
+
+    void Start()
+    {
+        inReach = false;
+        pickUpText.SetActive(false);
+        invOB.SetActive(false);
+
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+
+        }
+
+        //inventory
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //Add item -_-
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform);
+                   
+                    break;
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+
+        }
+    }
+
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("E"))
+        {
+            keyOB.SetActive(false);
+            keySound.Play();
+            invOB.SetActive(true);
+            pickUpText.SetActive(false);
+            itemButton.SetActive(true);
+        }
+
+        
+    }
+}
